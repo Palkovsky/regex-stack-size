226ae287427e592ca9447349a936cb79496bb389
Palkovsky <dawidmacek42@gmail.com>
2024-01-11 03:11:09 -0800

    Enable usage of regex-lite

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use regex::Regex;
 use rlimit::{getrlimit, setrlimit, Resource};
 use std::os::unix::process::CommandExt;
 
@@ -30,8 +29,25 @@ fn main() {
     }
 }
 
+#[cfg(feature = "regex")]
 fn eval_pattern(haystack: &str, pattern: &str) {
-    let regex = match Regex::new(&pattern) {
+    let regex = match regex::Regex::new(&pattern) {
+        Ok(r) => r,
+        Err(e) => {
+            eprintln!("Error compiling regex pattern: {}", e);
+            std::process::exit(1);
+        }
+    };
+    if regex.is_match(&haystack) {
+        println!("The haystack matches the regex pattern.");
+    } else {
+        println!("The haystack does not match the regex pattern.");
+    }
+}
+
+#[cfg(feature = "lite")]
+fn eval_pattern(haystack: &str, pattern: &str) {
+    let regex = match regex_lite::Regex::new(&pattern) {
         Ok(r) => r,
         Err(e) => {
             eprintln!("Error compiling regex pattern: {}", e);
